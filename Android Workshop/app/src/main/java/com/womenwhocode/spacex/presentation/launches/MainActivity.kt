6e1203d787ecc5d8c7d81c8b3bbfc856048b154c
Rocio Ortega <rocioortegapriego@gmail.com>
2018-05-19 12:09:06 +0200

    Create base SpaceX project

---
+++ Android Workshop/app/src/main/java/com/womenwhocode/spacex/presentation/launches/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.womenwhocode.spacex.presentation.launches
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.womenwhocode.spacex.R
+
+/**
+ * Created by Rocio Ortega on 19/05/2018.
+ */
+class MainActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
